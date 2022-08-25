27f6a76bfc3993d7d21a7f9b539a14e2134fa189
Kamil Jarczak <kamilj12@gmail.com>
2022-08-25 21:36:26 +0200

    Adding background, jump for player

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -5,4 +5,18 @@ canvas.width=innerWidth*0.9
 canvas.height=innerHeight*0.9
 canvas.style.borderStyle="solid"
 canvas.style.background="./img/backgroundMeadow.jpg"
+canvasRefresh();
 }
+function canvasRefresh(){
+  window.requestAnimationFrame(canvasRefresh)
+  //canvasContext.clearRect(0,0,canvas.width, canvas.height);
+  drawBgImg();
+}
+function drawBgImg() {
+  let bgImg = new Image();
+  bgImg.src = '/src/img/backgroundMeadow.jpg';
+  bgImg.onload = () => {
+    canvasContext.drawImage(bgImg, 0, 0, canvas.width, canvas.height);
+  }
+}
+;
--- src/js/fruit.js
+++ src/js/fruit.js
@@ -1,8 +1,12 @@
 import {object} from "./object.js";
+import {canvas, canvasContext} from "./canvas.js";
 export class fruit extends object{
-  constructor(canvasContext, x, y, width,height)
+  constructor(canvasContext, x, y, width,height,points)
   {
-   super(canvasContext, x, y, width,height)
+   super(canvasContext, x, y, width,height,points)
+   console.log(Math.random(0,canvas.width))
   }
 
 }
+const fruits=[]
+fruits.push(new fruit(canvasContext,Math.random()*canvas.width,0,20,20))
