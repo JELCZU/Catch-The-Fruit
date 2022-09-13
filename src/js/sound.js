8c19b97202a99df3da17d4abc28c635bd0d638d2
Kamil Jarczak <kamilj12@gmail.com>
2022-09-13 15:39:14 +0200

    added index.css added background of website canvas is centered horrizontaly and verticaly, added song while losing game, added button start game but it's not working yet

--- src/js/sound.js
+++ src/js/sound.js
@@ -3,3 +3,8 @@ export function playGetPointMusic()
   let audio=new Audio("/src/audio/getPoint.mp3")
   audio.play();
 }
+export function playLoseGameMusic()
+{
+  let audio=new Audio("/src/audio/loseGame.mp3")
+  audio.play();
+}
